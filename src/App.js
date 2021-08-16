0b90c438a613d0a7cb1d2f3ea9a96027fd27412d
heatclift77 <adityapratamaputra74@gmail.com>
2021-08-16 14:33:52 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 
 function App() {
+  const [quote, setQuote] = useState('')
+  const [myQuote, setMyQuote] = useState('')
+  // favourite quotes
+  const [favouriteQuotes, setFavouriteQuotes] = useState([])
+
+  useEffect(()=>{
+    axios.get('https://api.kanye.rest')
+    .then(res=>{
+      setQuote(res.data.quote)
+    })
+  },[])
+  
+  function getQuote(){
+    axios.get('https://api.kanye.rest')
+    .then(res=>{
+      setQuote(res.data.quote)
+    })
+  }
+
+  function addQuotes(){
+    if(favouriteQuotes.length === 0){
+      setFavouriteQuotes([...favouriteQuotes, quote])
+    }else{
+      let cekQuote = favouriteQuotes.filter(data=>{
+        return data === quote
+      })
+
+      if(cekQuote.length === 0){
+        setFavouriteQuotes([...favouriteQuotes, quote])
+      }else{
+        // nothing to do
+      }
+    }
+  }
+
+  function typeMyQuote(e){
+    setMyQuote(e.target.value)
+  }
+
+  function addMyQuote(){
+    if(favouriteQuotes.length === 0){
+      setFavouriteQuotes([...favouriteQuotes, myQuote])
+    }else{
+      let cekQuote = favouriteQuotes.filter(data=>{
+        return data === myQuote
+      })
+
+      if(cekQuote.length === 0){
+        setFavouriteQuotes([...favouriteQuotes, myQuote])
+        
+        // reset input value after success adding quote 
+        setMyQuote('')
+      }else{
+        // nothing to do
+      }
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="container">
+      <div className="row">
+        <div classname="col-8 mx-auto">
+          <h1>Kanye-West Quote</h1>
+        </div>
+        <div classname="col-8 mx-auto">
+          <h3>{quote}</h3>
+          <div classname="d-flex">
+            <button onClick={getQuote}>Get Quote</button>
+            <button onClick={addQuotes}>Add Favourite</button>
+          </div>
+          <ul>
+            {favouriteQuotes.map(quote=>{
+              return <li><q>{quote}</q></li>
+            })}
+          </ul>
+          <div>
+            <h2>My Quotes</h2>
+            <div>
+              <input type="text" onChange={typeMyQuote} value={myQuote} />
+              <button onClick={addMyQuote}>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
